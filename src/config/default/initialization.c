3af5de8bf34cbae653f19fe1296345ca689b6353
Dennis Sarsozo <dennis.sarsozo+github@gmail.com>
2025-06-19 10:22:08 -1000

    formatting

--- src/config/default/initialization.c
+++ src/config/default/initialization.c
@@ -15,26 +15,26 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
-*
-* Subject to your compliance with these terms, you may use Microchip software
-* and any derivatives exclusively with Microchip products. It is your
-* responsibility to comply with third party license terms applicable to your
-* use of third party software (including open source software) that may
-* accompany Microchip software.
-*
-* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-* PARTICULAR PURPOSE.
-*
-* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ * Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software
+ * and any derivatives exclusively with Microchip products. It is your
+ * responsibility to comply with third party license terms applicable to your
+ * use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+ * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
  *******************************************************************************/
 // DOM-IGNORE-END
 
@@ -46,7 +46,6 @@
 #include "configuration.h"
 #include "definitions.h"
 #include "device.h"
-
 
 // ****************************************************************************
 // ****************************************************************************
@@ -69,9 +68,6 @@
 #pragma config WDT_WEN = CLEAR
 #pragma config NVMCTRL_REGION_LOCKS = 0xffffffffU
 
-
-
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Driver Initialization Data
@@ -87,29 +83,26 @@
 /* SDSPI Client Objects Pool */
 static DRV_SDSPI_CLIENT_OBJ drvSDSPI0ClientObjPool[DRV_SDSPI_CLIENTS_NUMBER_IDX0];
 
-
-
 /* SDSPI Driver Initialization Data */
 static const DRV_SDSPI_INIT drvSDSPI0InitData =
-{
-    .spiDrvIndex            = 0,
+    {
+        .spiDrvIndex = 0,
 
-    /* SDSPI Number of clients */
-    .numClients             = DRV_SDSPI_CLIENTS_NUMBER_IDX0,
+        /* SDSPI Number of clients */
+        .numClients = DRV_SDSPI_CLIENTS_NUMBER_IDX0,
 
-    /* SDSPI Client Objects Pool */
-    .clientObjPool          = (uintptr_t)&drvSDSPI0ClientObjPool[0],
+        /* SDSPI Client Objects Pool */
+        .clientObjPool = (uintptr_t)&drvSDSPI0ClientObjPool[0],
 
+        .chipSelectPin = DRV_SDSPI_CHIP_SELECT_PIN_IDX0,
 
-    .chipSelectPin          = DRV_SDSPI_CHIP_SELECT_PIN_IDX0,
+        .sdcardSpeedHz = DRV_SDSPI_SPEED_HZ_IDX0,
 
-    .sdcardSpeedHz          = DRV_SDSPI_SPEED_HZ_IDX0,
+        .pollingIntervalMs = DRV_SDSPI_POLLING_INTERVAL_MS_IDX0,
 
-    .pollingIntervalMs      = DRV_SDSPI_POLLING_INTERVAL_MS_IDX0,
+        .writeProtectPin = SYS_PORT_PIN_NONE,
 
-    .writeProtectPin        = SYS_PORT_PIN_NONE,
-
-    .isFsEnabled            = true,
+        .isFsEnabled = true,
 
 };
 // </editor-fold>
@@ -135,33 +128,30 @@ static const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SERCOM2_SPI_CallbackRegister,
 };
 
-static const uint32_t drvSPI0remapDataBits[]= { 0x0, 0x1, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU };
-static const uint32_t drvSPI0remapClockPolarity[] = { 0x0, 0x20000000 };
-static const uint32_t drvSPI0remapClockPhase[] = { 0x10000000, 0x0 };
+static const uint32_t drvSPI0remapDataBits[] = {0x0, 0x1, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU, 0xFFFFFFFFU};
+static const uint32_t drvSPI0remapClockPolarity[] = {0x0, 0x20000000};
+static const uint32_t drvSPI0remapClockPhase[] = {0x10000000, 0x0};
 
 /* SPI Driver Initialization Data */
 static const DRV_SPI_INIT drvSPI0InitData =
-{
-    /* SPI PLIB API */
-    .spiPlib = &drvSPI0PlibAPI,
+    {
+        /* SPI PLIB API */
+        .spiPlib = &drvSPI0PlibAPI,
 
-    .remapDataBits = drvSPI0remapDataBits,
+        .remapDataBits = drvSPI0remapDataBits,
 
-    .remapClockPolarity = drvSPI0remapClockPolarity,
+        .remapClockPolarity = drvSPI0remapClockPolarity,
 
-    .remapClockPhase = drvSPI0remapClockPhase,
+        .remapClockPhase = drvSPI0remapClockPhase,
 
-    /* SPI Number of clients */
-    .numClients = DRV_SPI_CLIENTS_NUMBER_IDX0,
+        /* SPI Number of clients */
+        .numClients = DRV_SPI_CLIENTS_NUMBER_IDX0,
 
-    /* SPI Client Objects Pool */
-    .clientObjPool = (uintptr_t)&drvSPI0ClientObjPool[0],
-
+        /* SPI Client Objects Pool */
+        .clientObjPool = (uintptr_t)&drvSPI0ClientObjPool[0],
 
 };
 // </editor-fold>
-
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -178,63 +168,52 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // <editor-fold defaultstate="collapsed" desc="File System Initialization Data">
 
-
 const SYS_FS_MEDIA_MOUNT_DATA sysfsMountTable[SYS_FS_VOLUME_NUMBER] =
-{
-    {NULL}
-};
+    {
+        {NULL}};
 
 static const SYS_FS_FUNCTIONS FatFsFunctions =
-{
-    .mount             = FATFS_mount,
-    .unmount           = FATFS_unmount,
-    .open              = FATFS_open,
-    .read_t              = FATFS_read,
-    .close             = FATFS_close,
-    .seek              = FATFS_lseek,
-    .fstat             = FATFS_stat,
-    .getlabel          = FATFS_getlabel,
-    .currWD            = FATFS_getcwd,
-    .getstrn           = FATFS_gets,
-    .openDir           = FATFS_opendir,
-    .readDir           = FATFS_readdir,
-    .closeDir          = FATFS_closedir,
-    .chdir             = FATFS_chdir,
-    .chdrive           = FATFS_chdrive,
-    .write_t             = FATFS_write,
-    .tell              = FATFS_tell,
-    .eof               = FATFS_eof,
-    .size              = FATFS_size,
-    .mkdir             = FATFS_mkdir,
-    .remove_t            = FATFS_unlink,
-    .setlabel          = FATFS_setlabel,
-    .truncate          = FATFS_truncate,
-    .chmode            = FATFS_chmod,
-    .chtime            = FATFS_utime,
-    .rename_t            = FATFS_rename,
-    .sync              = FATFS_sync,
-    .putchr            = FATFS_putc,
-    .putstrn           = FATFS_puts,
-    .formattedprint    = FATFS_printf,
-    .testerror         = FATFS_error,
-    .formatDisk        = (FORMAT_DISK)FATFS_mkfs,
-    .partitionDisk     = FATFS_fdisk,
-    .getCluster        = FATFS_getclusters
-};
-
-
-
-
-static const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
-{
     {
-        .nativeFileSystemType = FAT,
-        .nativeFileSystemFunctions = &FatFsFunctions
-    }
-};
+        .mount = FATFS_mount,
+        .unmount = FATFS_unmount,
+        .open = FATFS_open,
+        .read_t = FATFS_read,
+        .close = FATFS_close,
+        .seek = FATFS_lseek,
+        .fstat = FATFS_stat,
+        .getlabel = FATFS_getlabel,
+        .currWD = FATFS_getcwd,
+        .getstrn = FATFS_gets,
+        .openDir = FATFS_opendir,
+        .readDir = FATFS_readdir,
+        .closeDir = FATFS_closedir,
+        .chdir = FATFS_chdir,
+        .chdrive = FATFS_chdrive,
+        .write_t = FATFS_write,
+        .tell = FATFS_tell,
+        .eof = FATFS_eof,
+        .size = FATFS_size,
+        .mkdir = FATFS_mkdir,
+        .remove_t = FATFS_unlink,
+        .setlabel = FATFS_setlabel,
+        .truncate = FATFS_truncate,
+        .chmode = FATFS_chmod,
+        .chtime = FATFS_utime,
+        .rename_t = FATFS_rename,
+        .sync = FATFS_sync,
+        .putchr = FATFS_putc,
+        .putstrn = FATFS_puts,
+        .formattedprint = FATFS_printf,
+        .testerror = FATFS_error,
+        .formatDisk = (FORMAT_DISK)FATFS_mkfs,
+        .partitionDisk = FATFS_fdisk,
+        .getCluster = FATFS_getclusters};
+
+static const SYS_FS_REGISTRATION_TABLE sysFSInit[SYS_FS_MAX_FILE_SYSTEM_TYPE] =
+    {
+        {.nativeFileSystemType = FAT,
+         .nativeFileSystemFunctions = &FatFsFunctions}};
 // </editor-fold>
-
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -252,14 +231,12 @@ static const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
 };
 
 static const SYS_TIME_INIT sysTimeInitData =
-{
-    .timePlib = &sysTimePlibAPI,
-    .hwTimerIntNum = RTC_IRQn,
+    {
+        .timePlib = &sysTimePlibAPI,
+        .hwTimerIntNum = RTC_IRQn,
 };
 
 // </editor-fold>
-
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -278,17 +255,16 @@ static const SYS_TIME_INIT sysTimeInitData =
  ********************************************************************************/
 static void STDIO_BufferModeSet(void)
 {
-    /* MISRAC 2012 deviation block start */
-    /* MISRA C-2012 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2012_R_21_6_DR_3 */
+  /* MISRAC 2012 deviation block start */
+  /* MISRA C-2012 Rule 21.6 deviated 2 times in this file.  Deviation record ID -  H3_MISRAC_2012_R_21_6_DR_3 */
 
-    /* Make stdin unbuffered */
-    setbuf(stdin, NULL);
+  /* Make stdin unbuffered */
+  setbuf(stdin, NULL);
 
-    /* Make stdout unbuffered */
-    setbuf(stdout, NULL);
-    /* MISRAC 2012 deviation block end */
+  /* Make stdout unbuffered */
+  setbuf(stdout, NULL);
+  /* MISRAC 2012 deviation block end */
 }
-
 
 /* MISRAC 2012 deviation block end */
 
@@ -302,67 +278,57 @@ static void STDIO_BufferModeSet(void)
   Remarks:
  */
 
-void SYS_Initialize ( void* data )
+void SYS_Initialize(void *data)
 {
 
-    /* MISRAC 2012 deviation block start */
-    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+  /* MISRAC 2012 deviation block start */
+  /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
-    NVMCTRL_Initialize( );
+  NVMCTRL_Initialize();
 
-    STDIO_BufferModeSet();
+  STDIO_BufferModeSet();
 
+  PORT_Initialize();
 
-  
-    PORT_Initialize();
+  CLOCK_Initialize();
 
-    CLOCK_Initialize();
+  SERCOM3_USART_Initialize();
 
+  SERCOM2_SPI_Initialize();
 
+  EVSYS_Initialize();
 
+  SERCOM4_I2C_Initialize();
 
-    SERCOM3_USART_Initialize();
+  RTC_Initialize();
 
-    SERCOM2_SPI_Initialize();
+  /* MISRAC 2012 deviation block start */
+  /* Following MISRA-C rules deviated in this block  */
+  /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+  /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
-    EVSYS_Initialize();
+  /* Initialize SDSPI0 Driver Instance */
+  sysObj.drvSDSPI0 = DRV_SDSPI_Initialize(DRV_SDSPI_INDEX_0, (SYS_MODULE_INIT *)&drvSDSPI0InitData);
 
-    SERCOM4_I2C_Initialize();
+  /* Initialize SPI0 Driver Instance */
+  sysObj.drvSPI0 = DRV_SPI_Initialize(DRV_SPI_INDEX_0, (SYS_MODULE_INIT *)&drvSPI0InitData);
 
-    RTC_Initialize();
+  /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -
+  H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
 
+  sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
 
-    /* MISRAC 2012 deviation block start */
-    /* Following MISRA-C rules deviated in this block  */
-    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
-    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+  /* MISRAC 2012 deviation block end */
 
-    /* Initialize SDSPI0 Driver Instance */
-    sysObj.drvSDSPI0 = DRV_SDSPI_Initialize(DRV_SDSPI_INDEX_0, (SYS_MODULE_INIT *)&drvSDSPI0InitData);
+  /*** File System Service Initialization Code ***/
+  (void)SYS_FS_Initialize((const void *)sysFSInit);
 
-    /* Initialize SPI0 Driver Instance */
-    sysObj.drvSPI0 = DRV_SPI_Initialize(DRV_SPI_INDEX_0, (SYS_MODULE_INIT *)&drvSPI0InitData);
+  /* MISRAC 2012 deviation block end */
+  APP_Initialize();
 
+  NVIC_Initialize();
 
-    /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
-    H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
-        
-    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
-    
-    /* MISRAC 2012 deviation block end */
-
-    /*** File System Service Initialization Code ***/
-    (void) SYS_FS_Initialize( (const void *) sysFSInit );
-
-
-    /* MISRAC 2012 deviation block end */
-    APP_Initialize();
-
-
-    NVIC_Initialize();
-
-
-    /* MISRAC 2012 deviation block end */
+  /* MISRAC 2012 deviation block end */
 }
 
 /*******************************************************************************
