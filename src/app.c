17a7daa80789aac74fe7bff5251186b96b141b0b
Dennis Sarsozo <dennis.sarsozo+github@gmail.com>
2025-06-23 15:07:20 -1000

    remove blinking led logic for now

--- src/app.c
+++ src/app.c
@@ -21,30 +21,30 @@
     files.
  *******************************************************************************/
 
-//DOM-IGNORE-BEGIN
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
-*
-* Subject to your compliance with these terms, you may use Microchip software
-* and any derivatives exclusively with Microchip products. It is your
-* responsibility to comply with third party license terms applicable to your
-* use of third party software (including open source software) that may
-* accompany Microchip software.
-*
-* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-* PARTICULAR PURPOSE.
-*
-* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
-//DOM-IGNORE-END
+ * Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software
+ * and any derivatives exclusively with Microchip products. It is your
+ * responsibility to comply with third party license terms applicable to your
+ * use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+ * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
 
 // *****************************************************************************
 // *****************************************************************************
@@ -63,13 +63,13 @@
 // *****************************************************************************
 // *****************************************************************************
 
-#define SDCARD_MOUNT_NAME    "/mnt/myDrive1"
-#define SDCARD_DEV_NAME      "/dev/mmcblka1"
-#define SDCARD_FILE_NAME     "hello.txt"
-#define SDCARD_DIR_NAME      "Dir1"
+#define SDCARD_MOUNT_NAME "/mnt/myDrive1"
+#define SDCARD_DEV_NAME "/dev/mmcblka1"
+#define SDCARD_FILE_NAME "hello.txt"
+#define SDCARD_DIR_NAME "Dir1"
 
-#define APP_DATA_LEN         512
-    
+#define APP_DATA_LEN 512
+
 char APP_INIT_MSG[] = "\r\nAPP INITIALIZED\r\n";
 char SD_MOUNT_FAIL[] = "SD MOUNT FAIL\r\n";
 char SD_SUCCESS_MSG[] = "SD SUCCESS\r\n";
@@ -85,40 +85,16 @@ int data = 0;
 uint16_t rxCounter = 0;
 // *****************************************************************************
 
-void read_CMD(char* cmd);
+void read_CMD(char *cmd);
 void delay_ms(int delay);
 void USART_READ(void);
 void I2C_READ(void);
 void enableGPIOs(void);
 void disableGPIOs(void);
 void FATFS_APP(void);
-// Forward declaration for LED task used in tasks.c
-void LED_Task(void *pvParameters) __attribute__((used));
 
-FATFS FatFs;	/* FatFs work area needed for each volume */
-FIL Fil;		/* File object needed for each open file */
-
-// LED blink control for PDU packet processing
-static volatile bool pdu_processing = false;
-#define LED_RAPID_BLINK_MS 100
-
-void LED_Task(void *pvParameters)
-{
-    LED_OutputEnable();
-    while (true)
-    {
-        if (pdu_processing)
-        {
-            LED_Toggle();
-            vTaskDelay(pdMS_TO_TICKS(LED_RAPID_BLINK_MS));
-        }
-        else
-        {
-            LED_Set();
-            vTaskDelay(pdMS_TO_TICKS(50)); // Polling interval
-        }
-    }
-}
+FATFS FatFs; /* FatFs work area needed for each volume */
+FIL Fil;     /* File object needed for each open file */
 
 /*******************************************************************************
   Function:
@@ -128,20 +104,19 @@ void LED_Task(void *pvParameters)
     See prototype in app.h.
  */
 
-void APP_Initialize ( void )
+void APP_Initialize(void)
 {
-//    SERCOM3_USART_Write(&APP_INIT_MSG[0], sizeof(APP_INIT_MSG));
+    //    SERCOM3_USART_Write(&APP_INIT_MSG[0], sizeof(APP_INIT_MSG));
     disableGPIOs();
     RTC_Initialize();
     RTC_Timer32Start();
     SERCOM4_I2C_Initialize();
-    //SERCOM2_SPI_Initialize();
-    //set the LED solid color once initialized
+    // SERCOM2_SPI_Initialize();
+    // set the LED solid color once initialized
     LED_Set();
-    //set the LED solid color once initialized
+    // set the LED solid color once initialized
     LED_Set();
 }
-
 
 /******************************************************************************
   Function:
@@ -151,74 +126,86 @@ void APP_Initialize ( void )
     See prototype in app.h.
  */
 
-void APP_Tasks ( void )
+void APP_Tasks(void)
 {
     USART_READ();
-//    I2C_READ();
- //FATFS_APP();
+    //    I2C_READ();
+    // FATFS_APP();
 }
 
 UINT bw;
-void FATFS_APP(void) {
-    if (f_mount(&FatFs, "", 1) == FR_OK) {	/* Mount SD */
+void FATFS_APP(void)
+{
+    if (f_mount(&FatFs, "", 1) == FR_OK)
+    { /* Mount SD */
 
-		if (f_open(&Fil, "hello.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK) {	/* Open or create a file */
+        if (f_open(&Fil, "hello.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK)
+        { /* Open or create a file */
 
-			if ((Fil.fptr != 0) && (f_lseek(&Fil, Fil.fptr) != FR_OK)) goto endSD;	/* Jump to the end of the file */
+            if ((Fil.fptr != 0) && (f_lseek(&Fil, Fil.fptr) != FR_OK))
+                goto endSD; /* Jump to the end of the file */
 
             SERCOM3_USART_Write(&SD_SUCCESS_MSG[0], sizeof(SD_SUCCESS_MSG));
-			f_write(&Fil, "Hello world!\r\n", 14, &bw);	/* Write data to the file */
+            f_write(&Fil, "Hello world!\r\n", 14, &bw); /* Write data to the file */
 
-			endSD: f_close(&Fil);								/* Close the file */
-		} else {
+        endSD:
+            f_close(&Fil); /* Close the file */
+        }
+        else
+        {
             SERCOM3_USART_Write(&SD_FAILED_MSG[0], sizeof(SD_FAILED_MSG));
         }
-	} else {
+    }
+    else
+    {
         SERCOM3_USART_Write(&SD_MOUNT_FAIL[0], sizeof(SD_MOUNT_FAIL));
     }
 }
 
-void USART_READ(void) {    
+void USART_READ(void)
+{
     /* Check if there is a received character */
-        if(SERCOM3_USART_ReceiverIsReady() == true)
+    if (SERCOM3_USART_ReceiverIsReady() == true)
+    {
+        if (SERCOM3_USART_ErrorGet() == USART_ERROR_NONE)
         {
-            if(SERCOM3_USART_ErrorGet() == USART_ERROR_NONE)
+            SERCOM3_USART_Read(&data, 1);
+            //                data = SERCOM3_USART_ReadByte();
+
+            if (data == '\r' || data == '\n')
             {
-                SERCOM3_USART_Read(&data, 1);
-//                data = SERCOM3_USART_ReadByte();
+                SERCOM3_USART_Write("\0", 1);
+                //                    SERCOM3_USART_Write(&newline[0],sizeof(newline));
+                //                    SERCOM3_USART_Write(&receiveBuffer[0],rxCounter);
+                //                    SERCOM3_USART_Write("\r\n", 2);
+                //                    SERCOM3_USART_Write(&newline[0],sizeof(newline));
+                rxCounter = 0;
 
-                if(data == '\r' || data == '\n')
-                {
-                    SERCOM3_USART_Write("\0", 1);
-//                    SERCOM3_USART_Write(&newline[0],sizeof(newline));
-//                    SERCOM3_USART_Write(&receiveBuffer[0],rxCounter);
-//                    SERCOM3_USART_Write("\r\n", 2);
-//                    SERCOM3_USART_Write(&newline[0],sizeof(newline));
-                    rxCounter = 0;
-                    pdu_processing = true;
-                    decode_pdu_packet(receiveBuffer);
+                decode_pdu_packet(receiveBuffer);
 
-//                    read_CMD(receiveBuffer);
-                }
-                else
-                {
-                    receiveBuffer[rxCounter++] = data;
-                }
+                //                    read_CMD(receiveBuffer);
             }
             else
             {
-//                SERCOM3_USART_Write(&errorMessage[0],sizeof(errorMessage));
+                receiveBuffer[rxCounter++] = data;
             }
         }
+        else
+        {
+            //                SERCOM3_USART_Write(&errorMessage[0],sizeof(errorMessage));
+        }
+    }
 }
 
-void I2C_READ(void) {
+void I2C_READ(void)
+{
     uint8_t i2c_char = SERCOM4_I2C_ReadByte();
     SERCOM3_USART_WriteByte(i2c_char);
-    //SERCOM4_I2C_WriteByte(i2c_char);
+    // SERCOM4_I2C_WriteByte(i2c_char);
 }
 
-void enableGPIOs(void) {
+void enableGPIOs(void)
+{
     BURN_5V_Set();
     BURN2_EN_Set();
     SW_12V_EN1_Set();
@@ -229,7 +216,7 @@ void enableGPIOs(void) {
     SW_5V_EN3_Set();
     SW_5V_EN4_Set();
     SW_VBATT_EN_Set();
-//    WDT_WDI_Set();
+    //    WDT_WDI_Set();
     BURN1_EN_Set();
     IN1_Set();
     IN2_Set();
@@ -247,7 +234,8 @@ void enableGPIOs(void) {
     SLEEP2_Set();
 }
 
-void disableGPIOs(void) {
+void disableGPIOs(void)
+{
     BURN_5V_Clear();
     BURN2_EN_Clear();
     SW_12V_EN1_Clear();
@@ -258,7 +246,7 @@ void disableGPIOs(void) {
     SW_5V_EN3_Clear();
     SW_5V_EN4_Clear();
     SW_VBATT_EN_Clear();
-//    WDT_WDI_Clear();
+    //    WDT_WDI_Clear();
     BURN1_EN_Set();
     IN1_Clear();
     IN2_Clear();
@@ -276,76 +264,86 @@ void disableGPIOs(void) {
     SLEEP2_Clear();
 }
 
-void read_CMD(char *cmd) {   
-    if(strstr(cmd, "CMD: GPIO ON ALL"))
+void read_CMD(char *cmd)
+{
+    if (strstr(cmd, "CMD: GPIO ON ALL"))
         enableGPIOs();
-    else if(strstr(cmd, "CMD: GPIO OFF ALL"))
+    else if (strstr(cmd, "CMD: GPIO OFF ALL"))
         disableGPIOs();
-    else if(strstr(cmd, "CMD: SW_3V3_1 ENABLE"))
+    else if (strstr(cmd, "CMD: SW_3V3_1 ENABLE"))
         SW_3V3_EN1_Set();
-    else if(strstr(cmd, "CMD: SW_3V3_1 DISABLE"))
+    else if (strstr(cmd, "CMD: SW_3V3_1 DISABLE"))
         SW_3V3_EN1_Clear();
-    else if(strstr(cmd, "CMD: SW_3V3_2 ENABLE"))
+    else if (strstr(cmd, "CMD: SW_3V3_2 ENABLE"))
         SW_3V3_EN2_Set();
-    else if(strstr(cmd, "CMD: SW_3V3_2 DISABLE"))
+    else if (strstr(cmd, "CMD: SW_3V3_2 DISABLE"))
         SW_3V3_EN2_Clear();
-    else if(strstr(cmd, "CMD: SW_5V_1 ENABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_1 ENABLE"))
         SW_5V_EN1_Set();
-    else if(strstr(cmd, "CMD: SW_5V_1 DISABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_1 DISABLE"))
         SW_5V_EN1_Clear();
-    else if(strstr(cmd, "CMD: SW_5V_2 ENABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_2 ENABLE"))
         SW_5V_EN2_Set();
-    else if(strstr(cmd, "CMD: SW_5V_2 DISABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_2 DISABLE"))
         SW_5V_EN2_Clear();
-    else if(strstr(cmd, "CMD: SW_5V_3 ENABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_3 ENABLE"))
         SW_5V_EN3_Set();
-    else if(strstr(cmd, "CMD: SW_5V_3 DISABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_3 DISABLE"))
         SW_5V_EN3_Clear();
-    else if(strstr(cmd, "CMD: SW_5V_4 ENABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_4 ENABLE"))
         SW_5V_EN4_Set();
-    else if(strstr(cmd, "CMD: SW_5V_4 DISABLE"))
+    else if (strstr(cmd, "CMD: SW_5V_4 DISABLE"))
         SW_5V_EN4_Clear();
-    else if(strstr(cmd, "CMD: SW_12V ENABLE")) {
+    else if (strstr(cmd, "CMD: SW_12V ENABLE"))
+    {
         SW_12V_EN1_Set();
         SW_5V_EN4_Set();
     }
-    else if(strstr(cmd, "CMD: SW_12V DISABLE")) {
+    else if (strstr(cmd, "CMD: SW_12V DISABLE"))
+    {
         SW_12V_EN1_Clear();
         SW_5V_EN4_Clear();
     }
-    else if(strstr(cmd, "CMD: VBATT ENABLE"))
+    else if (strstr(cmd, "CMD: VBATT ENABLE"))
         SW_VBATT_EN_Set();
-    else if(strstr(cmd, "CMD: VBATT DISABLE"))
+    else if (strstr(cmd, "CMD: VBATT DISABLE"))
         SW_VBATT_EN_Clear();
-    else if(strstr(cmd, "CMD: WDT ENABLE"))
+    else if (strstr(cmd, "CMD: WDT ENABLE"))
         WDT_WDI_Set();
-    else if(strstr(cmd, "CMD: WDT DISABLE"))
+    else if (strstr(cmd, "CMD: WDT DISABLE"))
         WDT_WDI_Clear();
-    else if(strstr(cmd, "CMD: BURN ENABLE")) {
+    else if (strstr(cmd, "CMD: BURN ENABLE"))
+    {
         BURN1_EN_Set();
         BURN2_EN_Set();
         BURN_5V_Set();
     }
-    else if(strstr(cmd, "CMD: BURN DISABLE")) {
+    else if (strstr(cmd, "CMD: BURN DISABLE"))
+    {
         BURN1_EN_Clear();
         BURN2_EN_Clear();
         BURN_5V_Clear();
     }
-    else if(strstr(cmd, "CMD: BURN1 ENABLE")) {
+    else if (strstr(cmd, "CMD: BURN1 ENABLE"))
+    {
         BURN1_EN_Set();
         BURN_5V_Set();
     }
-    else if(strstr(cmd, "CMD: BURN1 DISABLE")) {
+    else if (strstr(cmd, "CMD: BURN1 DISABLE"))
+    {
         BURN1_EN_Clear();
     }
-    else if(strstr(cmd, "CMD: BURN2 ENABLE")) {
+    else if (strstr(cmd, "CMD: BURN2 ENABLE"))
+    {
         BURN2_EN_Set();
         BURN_5V_Set();
     }
-    else if(strstr(cmd, "CMD: BURN2 DISABLE")) {
+    else if (strstr(cmd, "CMD: BURN2 DISABLE"))
+    {
         BURN2_EN_Clear();
     }
-    else if(strstr(cmd, "CMD: HBRIDGE1 ENABLE")) {
+    else if (strstr(cmd, "CMD: HBRIDGE1 ENABLE"))
+    {
         FAULT1_Set();
         IN1_Set();
         IN2_Set();
@@ -353,8 +351,9 @@ void read_CMD(char *cmd) {
         IN4_Set();
         TRQ1_Set();
         SLEEP1_Set();
-    } 
-    else if(strstr(cmd, "CMD: HBRIDGE1 DISABLE")) {
+    }
+    else if (strstr(cmd, "CMD: HBRIDGE1 DISABLE"))
+    {
         FAULT1_Clear();
         IN1_Clear();
         IN2_Clear();
@@ -362,8 +361,9 @@ void read_CMD(char *cmd) {
         IN4_Clear();
         TRQ1_Clear();
         SLEEP1_Clear();
-    } 
-    else if(strstr(cmd, "CMD: HBRIDGE2 ENABLE")) {
+    }
+    else if (strstr(cmd, "CMD: HBRIDGE2 ENABLE"))
+    {
         FAULT2_Set();
         IN5_Set();
         IN6_Set();
@@ -371,8 +371,9 @@ void read_CMD(char *cmd) {
         IN8_Set();
         TRQ2_Set();
         SLEEP2_Set();
-    } 
-    else if(strstr(cmd, "CMD: HBRIDGE2 DISABLE")) {
+    }
+    else if (strstr(cmd, "CMD: HBRIDGE2 DISABLE"))
+    {
         FAULT2_Clear();
         IN5_Clear();
         IN6_Clear();
@@ -380,13 +381,14 @@ void read_CMD(char *cmd) {
         IN8_Clear();
         TRQ2_Clear();
         SLEEP2_Clear();
-    } 
-    else if(strstr(cmd, "CMD: FATFS"))
+    }
+    else if (strstr(cmd, "CMD: FATFS"))
         FATFS_APP();
 }
 
-void delay_ms(int delay) {
-    for(uint8_t i = 0; i < delay; i++)
+void delay_ms(int delay)
+{
+    for (uint8_t i = 0; i < delay; i++)
         asm("NOP");
 }
 
--- src/config/default/tasks.c
+++ src/config/default/tasks.c
@@ -21,26 +21,26 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
-*
-* Subject to your compliance with these terms, you may use Microchip software
-* and any derivatives exclusively with Microchip products. It is your
-* responsibility to comply with third party license terms applicable to your
-* use of third party software (including open source software) that may
-* accompany Microchip software.
-*
-* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-* PARTICULAR PURPOSE.
-*
-* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ * Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Subject to your compliance with these terms, you may use Microchip software
+ * and any derivatives exclusively with Microchip products. It is your
+ * responsibility to comply with third party license terms applicable to your
+ * use of third party software (including open source software) that may
+ * accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+ * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
  *******************************************************************************/
 // DOM-IGNORE-END
 
@@ -54,9 +54,6 @@
 #include "definitions.h"
 #include "sys_tasks.h"
 
-// Add extern declaration for LED_Task
-extern void LED_Task(void *pvParameters);
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: RTOS "Tasks" Routine
@@ -64,13 +61,13 @@ extern void LED_Task(void *pvParameters);
 // *****************************************************************************
 
 /* -------- constants -------------------------------------------------- */
-#define WATCHDOG_PERIOD_MS   500     // 0.5-s edges (well inside 10-s MAX16998 window)
+#define WATCHDOG_PERIOD_MS 500 // 0.5-s edges (well inside 10-s MAX16998 window)
 
 static void lWatchdogTask(void *pvParameters)
 {
     bool wdiLevel = false;
 
-    while(true)
+    while (true)
     {
         /* 1. clear internal SAME51 watchdog */
         WDT_Clear();
@@ -87,42 +84,34 @@ static void lWatchdogTask(void *pvParameters)
     }
 }
 
-static void lDRV_SDSPI_0_Tasks(  void *pvParameters  )
+static void lDRV_SDSPI_0_Tasks(void *pvParameters)
 {
-    while(true)
+    while (true)
     {
         DRV_SDSPI_Tasks(sysObj.drvSDSPI0);
         vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 }
 
-
-static void lSYS_FS_Tasks(  void *pvParameters  )
+static void lSYS_FS_Tasks(void *pvParameters)
 {
-    while(true)
+    while (true)
     {
         SYS_FS_Tasks();
         vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 }
 
-
-
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
 
-
-
-static void lAPP_Tasks(  void *pvParameters  )
-{   
-    while(true)
+static void lAPP_Tasks(void *pvParameters)
+{
+    while (true)
     {
         APP_Tasks();
     }
 }
-
-
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -137,77 +126,63 @@ static void lAPP_Tasks(  void *pvParameters  )
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-void SYS_Tasks ( void )
+void SYS_Tasks(void)
 {
     /* Maintain system services */
-    
-    (void) xTaskCreate( lSYS_FS_Tasks,
-        "SYS_FS_TASKS",
-        SYS_FS_STACK_SIZE,
-        (void*)NULL,
-        SYS_FS_PRIORITY ,
-        (TaskHandle_t*)NULL
-    );
+
+    (void)xTaskCreate(lSYS_FS_Tasks,
+                      "SYS_FS_TASKS",
+                      SYS_FS_STACK_SIZE,
+                      (void *)NULL,
+                      SYS_FS_PRIORITY,
+                      (TaskHandle_t *)NULL);
 
     /* Create watchdog task
      * This task maintains two watchdogs:
      * 1. Internal SAME51 watchdog (16s timeout)
-     * 
+     *
      * The task runs every 500ms to:
      * - Clear the internal watchdog timer
      * - Toggle the WDI pin for the external watchdog
-     * 
+     *
      * If this task fails to run:
      * - Internal watchdog will reset the MCU after ~16s
-     * 
+     *
      * Priority is set just above idle to ensure it runs even
      * when the system is idle, but doesn't interfere with
      * higher priority tasks.
      */
-    (void) xTaskCreate( lWatchdogTask,
-        "WDT",
-        configMINIMAL_STACK_SIZE,
-        NULL,
-        tskIDLE_PRIORITY + 1,
-        NULL
-    );
+    (void)xTaskCreate(lWatchdogTask,
+                      "WDT",
+                      configMINIMAL_STACK_SIZE,
+                      NULL,
+                      tskIDLE_PRIORITY + 1,
+                      NULL);
 
     /* Maintain Device Drivers */
-    (void) xTaskCreate( lDRV_SDSPI_0_Tasks,
-        "DRV_SD_0_TASKS",
-        DRV_SDSPI_STACK_SIZE_IDX0,
-        (void*)NULL,
-        DRV_SDSPI_PRIORITY_IDX0 ,
-        (TaskHandle_t*)NULL
-    );
+    (void)xTaskCreate(lDRV_SDSPI_0_Tasks,
+                      "DRV_SD_0_TASKS",
+                      DRV_SDSPI_STACK_SIZE_IDX0,
+                      (void *)NULL,
+                      DRV_SDSPI_PRIORITY_IDX0,
+                      (TaskHandle_t *)NULL);
 
     /* Maintain Middleware & Other Libraries */
-    
 
     /* Maintain the application's state machine. */
-    
-    /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate(
-           (TaskFunction_t) lAPP_Tasks,
-           "APP_Tasks",
-           1024,
-           NULL,
-           1U ,
-           &xAPP_Tasks);
 
-    /* Create LED blink/solid task */
-    (void) xTaskCreate(
-        LED_Task,
-        "LED_TASK",
-        configMINIMAL_STACK_SIZE,
+    /* Create OS Thread for APP_Tasks. */
+    (void)xTaskCreate(
+        (TaskFunction_t)lAPP_Tasks,
+        "APP_Tasks",
+        1024,
         NULL,
-        tskIDLE_PRIORITY + 2,
-        NULL
-    );
+        1U,
+        &xAPP_Tasks);
 
     /* Start RTOS Scheduler. */
-    
-     /**********************************************************************
+
+    /**********************************************************************
      * Create all Threads for APP Tasks before starting FreeRTOS Scheduler *
      ***********************************************************************/
     vTaskStartScheduler(); /* This function never returns. */
@@ -216,4 +191,3 @@ void SYS_Tasks ( void )
 /*******************************************************************************
  End of File
  */
-
